7cec4de9d192a46c56b4252def7764eda832970a
Eduard Carreras <ecarreras@gisce.net>
2019-11-26 15:10:12 +0100

    Bump to v0.7.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ INSTALL_REQUIRES = ['rq>=0.10.0', 'osconf']
 
 setup(
     name='autoworker',
-    version='0.7.1',
+    version='0.7.2',
     packages=find_packages(),
     url='https://github.com/gisce/autoworker',
     license='MIT',
